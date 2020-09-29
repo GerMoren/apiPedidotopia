03d7fe787cb11d630df74fd480805c9ff3bd3960
GerMoren <ingenieriamg91@gmail.com>
2020-09-29 00:54:45 -0300

    First commit

---
+++ src/models/Product.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("product", {
+    title: {
+      type: DataTypes.STRING,
+      // allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      // allowNull: false,
+    },
+    sku: {
+      type: DataTypes.STRING,
+      // allowNull: false,
+    },
+    stock_inicial: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    precio_inicial: {
+      type: DataTypes.REAL,
+      defaultValue: 0,
+    },
+    images: {
+      type: DataTypes.ARRAY(DataTypes.TEXT),
+    },
+  });
+};
