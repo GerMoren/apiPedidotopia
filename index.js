9c6334c173e27f688b4fbe069231adba16da0a18
GerMoren <ingenieriamg91@gmail.com>
2020-09-29 01:47:52 -0300

    cambio nodemon

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { server } = require("./src/app.js");
 // const { conn } = require("./src/db.js");
 
 const port = process.env.PORT || 8080;
+console.log(port);
 // Syncing all the models at once.
 // conn.sync({ force: true }).then(() => {
 server.listen(port, () => {
